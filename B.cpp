8686e1c59da82a618e434e68290920f1af5d4ca8
Taohid Al Farabi <34630218+tafarabi@users.noreply.github.com>
2018-10-05 16:55:44 +0600

    latest

--- B.cpp
+++ B.cpp
@@ -1,38 +1,33 @@
 
-// CPP program to find number of possible moves of knight
 #include <bits/stdc++.h>
 #define n 8
 #define m 8
 using namespace std;
 
-// To calculate possible moves
 int findPossibleMoves(int mat[n][m], int p, int q)
 {
-    // All possible moves of a knight
+
     int X[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
     int Y[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
     int count = 0;
 
-    // Check if each possible move is valid or not
     for (int i = 0; i < 8; i++) {
 
-        // Position of knight after move
+
         int x = p + X[i];
         int y = q + Y[i];
 
-        // count valid moves
-        if (x > 0 && y > 0 && x < n && y < m
+        if ((((p-x)*(p-x))+((q-y)*(q-y)))==5 && x > 0 && y > 0 && x < n && y < m
             && mat[x][y] == 0)
             count++;
 
+
     }
 
-    // Return number of possible moves
     return count;
 }
 
-// Driver program to check findPossibleMoves()
 int main()
 {
     int mat[n][m] = { { 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -46,15 +41,16 @@ int main()
                        };
 
     int p, q,t;
-
-
-
     cin>>t;
-
+if(t>=1 && t<=64)
+{
     for(int i=0;i<t;i++){
     cin>>p>>q;
+    if(p>=1&&p<=8&&q>=1&&q<=8)
     printf("Case %d: %d\n", i+1, findPossibleMoves(mat, p, q));
     }
+}
+
 
 
     return 0;
