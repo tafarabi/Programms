0b5e626f7fc81dc6669da255d0c18c0427aa6c6e
Taohid Al Farabi <34630218+tafarabi@users.noreply.github.com>
2018-10-05 20:40:39 +0600

    c

--- C.cpp
+++ C.cpp
@@ -2,7 +2,7 @@
 long long int printDivisors(long long int n)
 {
     long long int countno=0;
-    for (int i=1;i<=n;i++)
+    for (int i=1; i<=n/2; i++)
         if (n%i==0)
         {
             if(i%2!=0)
@@ -11,9 +11,22 @@ long long int printDivisors(long long int n)
             }
 
         }
-             return countno;
+    return (countno);
 }
+int printNos(unsigned int n,int n1)
+{
 
+    long long int k=0;
+    if(n >= n1)
+    {
+        printNos(n-1,n1);
+        // printf("%d ", n);
+        k=printDivisors(n);
+        printf("%lld  ",k);
+
+    }
+    return k;
+}
 int main()
 {
 
@@ -22,20 +35,16 @@ int main()
 
 
     if(t>0&&t<=100000)
-for(long long int p=0;p<t;p++)
-{
-    long long int cf=0;
-    scanf("%lld %lld",&l,&r);
-    for(long long int j=l;j<=r;j++)
-    {
-       long long int k=printDivisors(j);
-        if(k%2!=0)
-           cf++;
+        for(long long int p=0; p<t; p++)
+        {
+            long long int cf=0;
+            scanf("%lld %lld",&l,&r);
 
-    }
-printf("Case %lld:%lld \n",p+1,cf);
+            cf = printNos(r,l);
 
-}
- return 0;
+            printf("Case %lld: %lld \n",p+1,cf);
+
+        }
+    return 0;
 
 }
